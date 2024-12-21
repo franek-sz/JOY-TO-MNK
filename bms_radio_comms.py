af039f0d800a3c66c937260813c4efd7d8419dbc
szawifr <93454655+szawifr@users.noreply.github.com>
2024-12-22 00:54:52 +0100

    added joy initialization

--- bms_radio_comms.py
+++ bms_radio_comms.py
@@ -6,21 +6,23 @@ pygame.init()
 
 pygame.joystick.init()
 
-# Put the button IDs here.
-btnid1 = 10
-btnid2 = 11
-btnid3 = 12
+# # Put the button IDs here.
+# btnid1 = 10
+# btnid2 = 11
+# btnid3 = 12
 
-# Put the keys to be pressed here.
-key1 = 'q' #awacs
-key2 = 't' #atc
-key3 = 'y' #other agencies
+# # Put the keys to be pressed here.
+# key1 = 'q' #awacs
+# key2 = 't' #atc
+# key3 = 'y' #other agencies
 
-# button_key_map = {
-#     13: 't',
-#     15: 'y',
-#     16: 'q',
-# }
+button_key_map = {
+    13: 't',
+    15: 'y',
+    16: 'q',
+}
+
+joysticks = {}
 
 # Put the name of the joystick you want the script to read.
 device_name = 'WINWING Orion Throttle Base II + F15EX HANDLE L + F15EX HANDLE R'
@@ -39,6 +41,16 @@ for i in range(joystick_count):
     print(f"Joystick {i + 1}: {joystick.get_name()}")
     print(f"Joystick Instance Id: {joystick.get_guid()}")
     print(f"Joystick GUID: {joystick.get_instance_id()}")
+
+for event in pygame.event.get():
+    if event.type == pygame.JOYDEVICEADDED:
+        joy = pygame.joystick.Joystick(event.device_index)
+        joysticks[joy.get_instance_id()] = joy
+        print(f"Joystick {joy.get_instance_id()} connencted")
+
+    if event.type == pygame.JOYDEVICEREMOVED:
+        del joysticks[event.instance_id]
+        print(f"Joystick {event.instance_id} disconnected")
 
 try:
     print(f"Ctrl + C to exit!")
